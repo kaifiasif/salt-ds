3dd12c09c630dca6035f7b8358b7d7d7177c9b54
Alina Visan <alycry.alina@gmail.com>
2024-02-01 16:43:01 +0200

    Update MenuGroup.tsx

--- packages/lab/src/menu/MenuGroup.tsx
+++ packages/lab/src/menu/MenuGroup.tsx
@@ -5,7 +5,7 @@ import { useWindow } from "@salt-ds/window";
 import { useComponentCssInjection } from "@salt-ds/styles";
 import menuGroupCss from "./MenuGroup.css";
 
-export interface OptionGroupProps extends ComponentPropsWithoutRef<"div"> {
+export interface MenuGroupProps extends ComponentPropsWithoutRef<"div"> {
   /**
    * The label of the option group.
    */
@@ -18,7 +18,7 @@ export interface OptionGroupProps extends ComponentPropsWithoutRef<"div"> {
 
 const withBaseName = makePrefixer("saltMenuGroup");
 
-export const MenuGroup = forwardRef<HTMLDivElement, OptionGroupProps>(
+export const MenuGroup = forwardRef<HTMLDivElement, MenuGroupProps>(
   function MenuGroup(props, ref) {
     const { className, children, label, ...rest } = props;
 
